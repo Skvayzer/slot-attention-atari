b5fe03e6e63d66335842831f311263af9bd3d8e4
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-03-01 12:29:31 +0300

    fixes

--- gym_agent/pytorch-DQN/common/wrappers.py
+++ gym_agent/pytorch-DQN/common/wrappers.py
@@ -47,10 +47,10 @@ class FireResetEnv(gym.Wrapper):
 
     def reset(self, **kwargs):
         self.env.reset(**kwargs)
-        obs, _, done, _, _ = self.env.step(1)
+        obs, _, done, _ = self.env.step(1)
         if done:
             self.env.reset(**kwargs)
-        obs, _, done, _, _ = self.env.step(2)
+        obs, _, done, _ = self.env.step(2)
         if done:
             self.env.reset(**kwargs)
         return obs

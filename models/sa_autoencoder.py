a24c7ecaa23e4e0be529cde7de48f5d9f4cf5806
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-03-07 00:46:52 +0300

    fixes

--- models/sa_autoencoder.py
+++ models/sa_autoencoder.py
@@ -21,11 +21,11 @@ class SlotAttentionAE(pl.LightningModule):
 
     def __init__(self,
                  resolution=(128, 128),
-                 num_slots=7,
+                 num_slots=10,
                  num_iters=3,
                  in_channels=3,
                  slot_size=64,
-                 hidden_size=64,
+                 hidden_size=32,
                  dataset='',
                  task='',
                  quantization=False,

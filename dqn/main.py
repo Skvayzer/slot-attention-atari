3fea4abc657faecdf450817e21729176b4e8e71b
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-03-08 02:29:34 +0300

    fixes

--- dqn/main.py
+++ dqn/main.py
@@ -236,14 +236,15 @@ if __name__ == '__main__':
 
     # create networks
     policy_net = DQN(n_actions=env.action_space.n).to(device)
-    policy_net.load_state_dict(torch.load("/home/sa_atari/dqn_seaquest_model_40000 (new)")['state_dict'])
+    ckpt = torch.load("/home/sa_atari/dqn_seaquest_model_40000 (new)")
+    policy_net.load_state_dict(ckpt['state_dict'])
     target_net = DQN(n_actions=env.action_space.n).to(device)
 
     target_net.load_state_dict(policy_net.state_dict())
 
     # setup optimizer
     optimizer = optim.Adam(policy_net.parameters(), lr=lr)
-
+    optimizer.load_state_dict(ckpt['optimizer'])
     # initialize replay memory
     memory = ReplayMemory(MEMORY_SIZE)
     

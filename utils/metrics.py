9eb90018987cce73d063396b0740202a1b1b8019
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-03-13 04:06:29 +0300

    fixes

--- utils/metrics.py
+++ utils/metrics.py
@@ -130,8 +130,8 @@ def mask_iou(
         others_mask = torch.concat((masks[:, :i], masks[:, i+1:]), dim=1).sum(dim=1)
         intersection = torch.matmul(others_mask, target_mask.t())
 
-        area1 = target_mask.sum(dim=2).view(-1, 1, -1)
-        area2 = others_mask.sum(dim=2).view(-1, 1, -1)
+        area1 = target_mask.sum(dim=1).view(-1, 1, -1)
+        area2 = others_mask.sum(dim=1).view(-1, 1, -1)
 
         union = (area1.t() + area2) - intersection
         iou += intersection / union

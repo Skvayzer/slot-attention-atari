ec85e37cc24673a5cd9ba871033dfa3df608aadd
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-03-18 02:09:27 +0300

    fixes

--- modules/pos_embeds.py
+++ modules/pos_embeds.py
@@ -10,8 +10,11 @@ class PosEmbeds(nn.Module):
     def __init__(self, hidden_size, resolution):
         super().__init__()
         self.linear = nn.Linear(4, hidden_size)
+        grid = torch.Tensor(build_grid(resolution))
+        print(f"\n\nATTENTION! GRID: {self.grid.shape} ", file=sys.stderr, flush=True)
 
-        self.grid = nn.Parameter(torch.Tensor(build_grid(resolution)), requires_grad=False)
+        self.grid = nn.Parameter(grid, requires_grad=False)
+
         
     def forward(self, inputs):
         pos_emb = self.linear(self.grid).moveaxis(3, 1)

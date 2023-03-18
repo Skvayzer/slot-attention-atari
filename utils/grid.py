e9f442827de875a2959eaf9de7e75edb830e9938
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-03-19 00:27:37 +0300

    fixes

--- utils/grid.py
+++ utils/grid.py
@@ -12,18 +12,18 @@ def build_grid(resolution):
 
     ranges = [np.linspace(0., 1., num=res) for res in resolution]
     grid = np.meshgrid(*ranges, sparse=False, indexing="ij")
-    print(f"\n\nATTENTION! GRID {grid}", file=sys.stderr, flush=True)
+    print(f"\n\nATTENTION! GRID {grid.shape}", file=sys.stderr, flush=True)
 
     grid = np.stack(grid, axis=-1)
-    print(f"\n\nATTENTION! GRID {grid}", file=sys.stderr, flush=True)
+    print(f"\n\nATTENTION! GRID {grid.shape}", file=sys.stderr, flush=True)
 
     grid = np.reshape(grid, [resolution[0], resolution[1], -1])
-    print(f"\n\nATTENTION! GRID {grid}", file=sys.stderr, flush=True)
+    print(f"\n\nATTENTION! GRID {grid.shape}", file=sys.stderr, flush=True)
 
     grid = np.expand_dims(grid, axis=0)
-    print(f"\n\nATTENTION! GRID {grid}", file=sys.stderr, flush=True)
+    print(f"\n\nATTENTION! GRID {grid.shape}", file=sys.stderr, flush=True)
 
     grid = grid.astype(np.float32)
-    print(f"\n\nATTENTION! GRID {grid}", file=sys.stderr, flush=True)
+    print(f"\n\nATTENTION! GRID {grid.shape}", file=sys.stderr, flush=True)
 
     return np.concatenate([grid, 1.0 - grid], axis=-1)

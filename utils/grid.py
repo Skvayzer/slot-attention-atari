a66c6c72b30da547d69f48ed04372bf4ca4dcd23
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-03-19 00:26:41 +0300

    fixes

--- utils/grid.py
+++ utils/grid.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 
 
@@ -6,10 +8,22 @@ def build_grid(resolution):
     :param resolution: tuple of 2 numbers
     :return: grid for positional embeddings built on input resolution
     """
+    print(f"\n\nATTENTION! BUILDING GRID ", file=sys.stderr, flush=True)
+
     ranges = [np.linspace(0., 1., num=res) for res in resolution]
     grid = np.meshgrid(*ranges, sparse=False, indexing="ij")
+    print(f"\n\nATTENTION! GRID {grid}", file=sys.stderr, flush=True)
+
     grid = np.stack(grid, axis=-1)
+    print(f"\n\nATTENTION! GRID {grid}", file=sys.stderr, flush=True)
+
     grid = np.reshape(grid, [resolution[0], resolution[1], -1])
+    print(f"\n\nATTENTION! GRID {grid}", file=sys.stderr, flush=True)
+
     grid = np.expand_dims(grid, axis=0)
+    print(f"\n\nATTENTION! GRID {grid}", file=sys.stderr, flush=True)
+
     grid = grid.astype(np.float32)
+    print(f"\n\nATTENTION! GRID {grid}", file=sys.stderr, flush=True)
+
     return np.concatenate([grid, 1.0 - grid], axis=-1)

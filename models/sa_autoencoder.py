e557f821cb75f4486c68caaace1834aec8ce9bcb
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-03-25 10:27:26 +0300

    fixes

--- models/sa_autoencoder.py
+++ models/sa_autoencoder.py
@@ -21,8 +21,8 @@ class InvariantSlotAttentionAE(pl.LightningModule):
 
     def __init__(self,
                  resolution=(128, 128),
-                 num_slots=10,
-                 val_num_slots=10,
+                 num_slots=20,
+                 val_num_slots=20,
                  num_iters=3,
                  in_channels=3,
                  slot_size=32,

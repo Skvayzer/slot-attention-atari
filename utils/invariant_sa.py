5b1bfa01ae859b0424f26c0d4231fa73b9f92229
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-03-30 04:51:16 +0300

    fixes

--- utils/invariant_sa.py
+++ utils/invariant_sa.py
@@ -9,11 +9,12 @@ def rot(angle):
                          [sin(angle), cos(angle)]])
 def postprocess(v1, v2):
     R = torch.concat((v1, v2), dim=0)
+    print(f"\n\nATTENTION! R: {R.shape} ", file=sys.stderr, flush=True)
+
     det = torch.linalg.det(R)
     if det < 0:
         v1, v2 = v2, v1
         R = torch.concat((v1, v2), dim=0)
-    print(f"\n\nATTENTION! R: {R.shape} ", file=sys.stderr, flush=True)
     angle = torch.arccos(R[0, 0])
     if angle > np.pi/4:
         return rot(np.pi/4)

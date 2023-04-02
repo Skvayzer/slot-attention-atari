afb0f754d59e6531cfb4aa32d1b7809e109f16e7
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-04-02 08:16:09 +0300

    fixes

--- models/sa_autoencoder.py
+++ models/sa_autoencoder.py
@@ -138,7 +138,7 @@ class InvariantSlotAttentionAE(pl.LightningModule):
         #         rel_grid_final[b, s, :, :] = (S_r_inverse[b, s, :, :] @ rel_grid[b, s, :, :].T).T
         print(f"\n\nATTENTION! before dec: {x.shape} ", file=sys.stderr, flush=True)
         print(f"\n\nATTENTION! self.h(rel_grid): {self.h(rel_grid_final).reshape(-1, self.hidden_size, *self.decoder_initial_size).shape} ", file=sys.stderr, flush=True)
-        return
+        
         x = self.decoder(x + self.h(rel_grid_final).reshape(-1, self.hidden_size, *self.decoder_initial_size))
 
         x = x.reshape(inputs.shape[0], num_slots, *x.shape[1:])

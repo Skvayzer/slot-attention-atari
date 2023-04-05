734ac62df6b44c328aaf0056210e6775bf257075
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-04-05 12:11:27 +0300

    fixes

--- models/sa_autoencoder.py
+++ models/sa_autoencoder.py
@@ -381,7 +381,7 @@ class SlotAttentionAE(pl.LightningModule):
         print(f"\n\nATTENTION! result image {result.shape} {result[0]} ", file=sys.stderr, flush=True)
 
         loss = F.mse_loss(result, imgs)
-        print(f"\n\nATTENTION! loss {loss.shape} {loss[0]} ", file=sys.stderr, flush=True)
+        print(f"\n\nATTENTION! loss {loss} ", file=sys.stderr, flush=True)
         return
         return loss, iou_loss
 

f837ce4d42d35b13a0c1ca54fffacd1815bb9ce5
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-04-18 18:34:08 +0300

    fixes

---
+++ datasets/source/download_waymo.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+#SBATCH --job-name=download_waymo
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+##SBATCH --time=0-0:05:00
+#SBATCH --mem-per-gpu=16GB
+#SBATCH --mail-user=k.smirnov@innopolis.university
+#SBATCH --mail-type=END
+#SBATCH --no-kill
+#SBATCH --comment="Обучение нейросетевой модели в рамках НИР Центра когнитивного моделирования"
+
+singularity instance start \
+                     --nv  \
+                     --bind /home/AI/yudin.da/smirnov_cv/sa_atari:/home/sa_atari \
+                     ml_env.sif ml_env
+
+singularity exec instance://ml_env /bin/bash -c "
+      source /miniconda/etc/profile.d/conda.sh;
+      conda activate ml_env;
+      pip install gcsfs waymo-open-dataset-tf-2-11-0==1.5.1;
+      set -x;
+      nvidia-smi;
+      free -m;
+      cd /home/sa_atari/datasets;
+      gsutil -m cp -r "gs://waymo_open_dataset_v_2_0_0/training" .;
+      free -m;
+";
+
+singularity instance stop ml_env

6e2f94e6ad857b7a321827ae758d7521969eed93
Konstantin Smirnov <mrskvayzer@gmail.com>
2023-05-04 00:04:59 +0300

    fixes

--- modules/encoder.py
+++ modules/encoder.py
@@ -1,5 +1,6 @@
 from torch import nn
 import torchvision
+import torch
 
 class Encoder(nn.Module):
     """
@@ -24,6 +25,7 @@ class WaymoEncoder(nn.Module):
         super().__init__()
         self.hidden_size = hidden_size
         self.resnet34 = torchvision.models.resnet34(pretrained=True) #torch.hub.load("pytorch/vision", "resnet34", weights="IMAGENET1K_V1")
+        self.resnet34 = self.replace_batchnorm(self.resnet34)
         num_ftrs = self.resnet34.fc.in_features
         self.resnet34.fc = nn.Linear(num_ftrs, hidden_size*16*24)
 
@@ -33,7 +35,15 @@ class WaymoEncoder(nn.Module):
         x = x.reshape(-1, self.hidden_size, 16, 24)
         return x
 
+    def replace_batchnorm(self, model):
+        for name, module in reversed(model._modules.items()):
+            if len(list(module.children())) > 0:
+                model._modules[name] = self.replace_batchnorm(module)
 
+            if isinstance(module, torch.nn.BatchNorm2d):
+                model._modules[name] = torch.nn.GroupNorm(32, module.num_features)
+
+        return model
 class Identity(nn.Module):
     def __init__(self):
         super(Identity, self).__init__()
